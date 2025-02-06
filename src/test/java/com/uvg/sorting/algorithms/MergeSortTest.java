7fb88f61dd98947aa876452804e38149c748e961
Tiffany24630 <sal24630@uvg.edu.gt>
2025-02-05 20:24:30 -0600

    Cambios y encabezado agregado en MergeSortTest.java

--- src/test/java/com/uvg/sorting/algorithms/MergeSortTest.java
+++ src/test/java/com/uvg/sorting/algorithms/MergeSortTest.java
@@ -1,52 +1,61 @@
 package com.uvg.sorting.algorithms;
 
+/*
+ * Universidad del Valle de Guatemala
+ * Autora: Tiffany Salazar Suarez
+ * Carnet: 24630
+ * Fecha de inicio: 5/02/2025
+ * Fecha de finalización: 5/02/2025
+ * Descripción: La clase MergeSortTest hace las pruebas unitarias de MergeSort
+*/
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class MergeSortTest {
+class MergeSortTest{
 
     @Test
-    void testMerge() {
+    void testMerge(){
         MergeSort mergeSort = new MergeSort();
         
-        // Caso de prueba con un arreglo de ejemplo
+        //Caso de prueba con un arreglo de ejemplo
         int[] array = {3, 5, 1, 2, 4, 6};
-        int[] expected = {1, 2, 3, 4, 5, 6};  // Resultado esperado después de la fusión
+        int[] expected = {1, 2, 3, 4, 5, 6};
 
-        // Subarreglos L y R para fusionar
-        mergeSort.merge(array, 0, 2, 5); // Fusiona los subarreglos [3, 5, 1] y [2, 4, 6]
+        //Subarreglos L y R para fusionar
+        mergeSort.merge(array, 0, 2, 5);
         
-        // Verificar que el arreglo esté correctamente fusionado
+        //Verificar que el arreglo esté correctamente fusionado
         assertArrayEquals(expected, array);
     }
 
     @Test
-    void testMergeSort() {
+    void testMergeSort(){
         MergeSort mergeSort = new MergeSort();
         
-        // Caso de prueba con un arreglo desordenado
+        //Caso de prueba con un arreglo desordenado
         int[] array = {3, 5, 1, 2, 4, 6};
-        int[] expected = {1, 2, 3, 4, 5, 6};  // Resultado esperado después de ordenar
+        int[] expected = {1, 2, 3, 4, 5, 6};
         
-        // Llamada al método merge_sort
+        //Llamada al método merge_sort
         mergeSort.merge_sort(array, 0, array.length - 1);
         
-        // Verificar que el arreglo esté ordenado correctamente
+        //Verificar que el arreglo esté ordenado correctamente
         assertArrayEquals(expected, array);
     }
 
     @Test
-    void testMergeSortAlreadySorted() {
+    void testMergeSortAlreadySorted(){
         MergeSort mergeSort = new MergeSort();
         
-        // Caso de prueba con un arreglo ya ordenado
+        //Caso de prueba con un arreglo ya ordenado
         int[] array = {1, 2, 3, 4, 5, 6};
-        int[] expected = {1, 2, 3, 4, 5, 6};  // El arreglo ya está ordenado
+        int[] expected = {1, 2, 3, 4, 5, 6};
         
-        // Llamada al método merge_sort
+        //Llamada al método merge_sort
         mergeSort.merge_sort(array, 0, array.length - 1);
         
-        // Verificar que el arreglo siga igual
+        //Verificar que el arreglo siga igual
         assertArrayEquals(expected, array);
     }
 }
