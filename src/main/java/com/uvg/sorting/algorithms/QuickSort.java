904a4416c5383a075fb83a734db36faf062ed568
Tiffany24630 <sal24630@uvg.edu.gt>
2025-02-05 20:20:41 -0600

    Clase QuickSort.java terminada

--- src/main/java/com/uvg/sorting/algorithms/QuickSort.java
+++ src/main/java/com/uvg/sorting/algorithms/QuickSort.java
@@ -1,5 +1,46 @@
 package com.uvg.sorting.algorithms;
 
-public class QuickSort {
-    
+/*
+ * Universidad del Valle de Guatemala
+ * Autora: Tiffany Salazar Suarez
+ * Carnet: 24630
+ * Fecha de inicio: 31/01/2025
+ * Fecha de finalización: 5/02/2025
+ * Descripción: La clase QuickSort se encarga de ordenar listas de números
+*/
+
+public class QuickSort{
+
+    //Método que parte el arreglo
+    public int partition(int[] arrayL, int peq, int alt){
+        int posPivvot = arrayL [alt];
+        int i = peq - 1;
+
+        //Reorganización de los elementos
+        for (int j = peq; j < alt; j++) {
+            if (arrayL[j] <= posPivvot) {
+                i++;
+                int temp = arrayL[i];
+                arrayL[i] = arrayL[j];
+                arrayL[j] = temp;
+            }
+        }
+
+        int temp = arrayL[i + 1];
+        arrayL[i + 1] = arrayL [alt];
+        arrayL [alt] = temp;
+
+        //Return con la posición del pivote
+        return i + 1;
+    }
+
+    //Método de QuickSort
+    public void quick_sort(int[] arrayL, int peq, int alt) {
+        if  (peq < alt) {
+            int posPiv = partition(arrayL, peq, alt);
+
+            quick_sort(arrayL, peq, posPiv - 1); //Subarreglo de la izquierda
+            quick_sort(arrayL, posPiv + 1, alt); //Subarreglo de la derecha
+        }
+    }
 }
